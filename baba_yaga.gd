9a1a30578344da1e8b3c6f492b97d3417792caab
Maciej Brzeski <maciej.brzeski.97@gmail.com>
2026-04-19 18:41:27 +0200

    game manager

--- baba_yaga.gd
+++ baba_yaga.gd
@@ -6,8 +6,10 @@ signal baba_hurt
 
 var max_hp := 3
 var current_hp := max_hp
+var baba_tween: Tween
 
 @onready var interactible_component: InteractibleComponent = %InteractibleComponent
+@onready var mesh: MeshInstance3D = $BabaYaga
 
 func hurt():
 	current_hp = current_hp - 1
@@ -16,4 +18,8 @@ func hurt():
 		game_over.emit()
 	else:
 		# play baba AAAH sound
+		baba_tween = get_tree().create_tween()
+		var material := mesh.get_surface_override_material(0) as StandardMaterial3D
+		baba_tween.tween_property(material, "albedo_color", Color.RED, 0.1)
+		baba_tween.tween_property(material, "albedo_color", Color.WHITE, 0.4)
 		baba_hurt.emit()
